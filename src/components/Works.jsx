622c04c50d91a32f2e695cff5deb20c57132293c
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2024-01-29 21:04:15 +0600

    Update Works.jsx
    .

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -43,7 +43,7 @@ const ProjectCard = ({
         </div>
       </div>
 
-      <div className="mt-5">
+      <div className="p-2 mt-5">
         <h3 className="text-white font-bold text-[24px]">{name}</h3>
         <p className="mt-2 text-secondary text-[14px] ">{description}</p>
       </div>
